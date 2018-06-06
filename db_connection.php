49b9f60d4626ce674bd496fe551283ae1863e8ac
Mike Ludemann <mike-ludemann@hotmail.de>
2018-06-06 22:26:24 +0200

    General updating files

--- db_connection.php
+++ db_connection.php
@@ -7,24 +7,18 @@ $password = "admin";
 
 // Create connection
 
-$connection = new mysqli_connect($servername, $database, $username, $password);
+$connection = new mysqli($servername, $username, $password, $database);
 
 // Checking connection
 
 if (!$connection) {
 
-    echo "<script>console.log('Fail: Could not connect to MySQL be built.');</script>";
-    echo "<script>console.log('Debug - Error Number: " . mysqli_connect_errno() . "');</script>";
-    echo "<script>console.log('Debug - Error Message: " . mysqli_connect_error() . "');</script>";
+    die("Connection failed - Error Number and Error Message: " . mysqli_connect_errno . " , " . mysqli_connect_error);
 
-    exit;
-
-    die("Connection failed: " . $connection->connect_error);
-
-} else {
+} /* else {
 
     echo "Connection successfull";
 
-}
+} */
 
 ?>
